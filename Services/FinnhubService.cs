8313554fd9e7ea3312d3ef236f144a82db6efa54
nbondili4 <nbondili4@US-C4B79S3>
2023-10-07 01:01:36 +0530

    Written Implementation for GetStockPriceQuote()

--- Services/FinnhubService.cs
+++ Services/FinnhubService.cs
@@ -1,4 +1,5 @@
 ﻿using ServiceContracts;
+using System.Text.Json;
 
 namespace Services
 {
@@ -17,7 +18,33 @@ namespace Services
 
         public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
         {
-            throw new NotImplementedException();
+            //create http client
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            //create http request
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol=MSFT&token=ckg0eq1r01qknh1jk6c0ckg0eq1r01qknh1jk6cg") //URI includes the secret token
+            };
+
+            //send request
+            HttpResponseMessage httpResponseMessage = httpClient.Send(httpRequestMessage);
+
+            //read response body
+            string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+            //convert response body (from JSON into Dictionary)
+            Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+            if (responseDictionary == null)
+                throw new InvalidOperationException("No response from server");
+
+            if (responseDictionary.ContainsKey("error"))
+                throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+            //return response dictionary back to the caller
+            return responseDictionary;
         }
     }
 }
