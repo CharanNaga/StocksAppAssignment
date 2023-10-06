023ae0e80f934ab40994c8d66730f1c71e4fb63b
nbondili4 <nbondili4@US-C4B79S3>
2023-10-07 00:28:58 +0530

    Created Folders and few items for StaticFiles, Controllers, Models and Views.

---
+++ StocksAppAssignment/Controllers/TradeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksAppAssignment.Controllers
+{
+    public class TradeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
