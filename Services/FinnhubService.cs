88dfcb05fa74764f8b496abc6932347420ab1e86
nbondili4 <nbondili4@US-C4B79S3>
2023-10-07 00:56:41 +0530

    Inheriting the interface to our Service Class Initially.

--- Services/FinnhubService.cs
+++ Services/FinnhubService.cs
@@ -1,6 +1,8 @@
-﻿namespace Services
+﻿using ServiceContracts;
+
+namespace Services
 {
-    public class FinnhubService
+    public class FinnhubService:IFinnhubService
     {
         private readonly IHttpClientFactory _httpClientFactory;
         public FinnhubService(IHttpClientFactory httpClientFactory)
@@ -8,5 +10,14 @@
             _httpClientFactory = httpClientFactory;
         }
 
+        public Dictionary<string, object>? GetCompanyProfile(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
