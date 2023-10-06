022dc6f5d63850239c7d64094015ca01dfe0afbd
nbondili4 <nbondili4@US-C4B79S3>
2023-10-07 01:26:48 +0530

    Added a new key in appSettings.json

--- StocksAppAssignment/Program.cs
+++ StocksAppAssignment/Program.cs
@@ -1,5 +1,9 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
