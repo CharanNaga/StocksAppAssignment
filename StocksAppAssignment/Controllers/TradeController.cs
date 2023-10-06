1b41dbd9a69cca28cbd256f46d676a29890eacf5
nbondili4 <nbondili4@US-C4B79S3>
2023-10-07 01:35:26 +0530

    Created TradingOptions class, added as a service and injected in controller

--- StocksAppAssignment/Controllers/TradeController.cs
+++ StocksAppAssignment/Controllers/TradeController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiceContracts;
 
 namespace StocksAppAssignment.Controllers
 {
     public class TradeController : Controller
     {
+        private readonly IFinnhubService _finnhubService;
+        private readonly IConfiguration _configuration;
+        private readonly TradingOptions _tradingOptions;
+
+        public TradeController(IFinnhubService finnhubService, IConfiguration configuration,IOptions<TradingOptions> tradingOptions)
+        {
+            _finnhubService = finnhubService;
+            _configuration = configuration;
+            _tradingOptions = tradingOptions.Value;
+        }
+
         [Route("/")]
+        [Route("[action]")]
+        [Route("~/[controller]")]
         public IActionResult Index()
         {
+            //reset stock symbol if not exists
+            if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
+                _tradingOptions.DefaultStockSymbol = "MSFT";
+
+            //get company profile from API server
+            Dictionary<string, object>? companyProfileDictionary = _finnhubService.GetCompanyProfile(_tradingOptions.DefaultStockSymbol);
+
+            //get stock price quotes from API server
+            Dictionary<string, object>? stockQuoteDictionary = _finnhubService.GetStockPriceQuote(_tradingOptions.DefaultStockSymbol);
             return View();
         }
     }
--- StocksAppAssignment/Program.cs
+++ StocksAppAssignment/Program.cs
@@ -1,8 +1,10 @@
 using ServiceContracts;
 using Services;
+using StocksAppAssignment;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
 builder.Services.AddHttpClient();
 
