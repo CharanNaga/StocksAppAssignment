514c2954979cc7b1a131b19aead43f4bab71cb48
nbondili4 <nbondili4@US-C4B79S3>
2023-12-11 13:52:15 +0530

    Created a new interface IStocksService and provided dummy implementation in new service class

---
+++ Services/StocksService.cs
@@ -0,0 +1,37 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class StocksService : IStocksService
+    {
+        private readonly List<BuyOrder> _buyOrders;
+        private readonly List<SellOrder> _sellOrders;
+
+        public StocksService()
+        {
+            _buyOrders = new List<BuyOrder>();
+            _sellOrders = new List<SellOrder>();
+        }
+        public Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BuyOrderResponse>> GetBuyOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<SellOrderResponse>> GetSellOrders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
