8639895c3b959ca1bb6bb4541a34fff383fe114c
nbondili4 <nbondili4@US-C4B79S3>
2023-12-11 12:23:33 +0530

    Created BuyOrder.cs in Entities Project & defined properties

---
+++ Entities/BuyOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class BuyOrder
+    {
+        public Guid BuyOrderID { get; set; }
+        [Required(ErrorMessage="Stock Symbol can't be blank")]
+        public string? StockSymbol { get; set; }
+        [Required(ErrorMessage ="Stock Name can't be blank")]
+        public string? StockName { get; set; }
+
+        public DateTime DateAndTimeOfOrder { get; set; }
+        [Range(1, 100000,ErrorMessage = "Can buy maximum of 100000 shares in single order, Minimum is 1.")]
+        public uint Quantity { get; set; }
+        [Range(1, 10000,ErrorMessage = "The maximum price of stock is 10000, Minimum is 1.")]
+        public double Price { get; set; }
+    }
+}
