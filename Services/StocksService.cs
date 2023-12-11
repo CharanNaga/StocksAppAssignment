b1838d5c925ab533fcc33fc038a0d809c63f5512
nbondili4 <nbondili4@US-C4B79S3>
2023-12-11 15:40:51 +0530

    Written actual logic for CreateBuyOrder() in Service

--- Services/StocksService.cs
+++ Services/StocksService.cs
@@ -1,6 +1,8 @@
 ﻿using Entities;
 using ServiceContracts;
 using ServiceContracts.DTO;
+using Services.Helpers;
+using System;
 
 namespace Services
 {
@@ -16,7 +18,24 @@ namespace Services
         }
         public Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
         {
-            throw new NotImplementedException();
+            //1. Check buyOrderRequest != null
+            if (buyOrderRequest == null)
+                throw new ArgumentNullException(nameof(buyOrderRequest));
+
+            //2. Validate all properties of buyOrderRequest
+            ValidationHelper.ModelValidation(buyOrderRequest); //validating all properties using Model validations by calling a reusable method.
+
+            //3. Convert BuyOrderRequest to BuyOrder type
+            BuyOrder buyOrder = buyOrderRequest.ToBuyOrder();
+
+            //4. Generate a new BuyOrderID
+            buyOrder.BuyOrderID = Guid.NewGuid();
+
+            //5. Then add it to the List<BuyOrder>
+            _buyOrders.Add(buyOrder);
+
+            //6. Return PersonResponse object with generated PersonID.
+            return Task.FromResult(buyOrder.ToBuyOrderResponse());
         }
 
         public Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
