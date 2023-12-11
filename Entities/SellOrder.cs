5dc76dfe2b895488bf19cd26dc0279f9c4c24cc9
nbondili4 <nbondili4@US-C4B79S3>
2023-12-11 12:29:27 +0530

    Created SellOrder.cs in Entities project & defined properties

---
+++ Entities/SellOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class SellOrder
+    {
+        public Guid SellOrderID { get; set; }
+        [Required(ErrorMessage = "Stock Symbol can't be blank")]
+        public string? StockSymbol { get; set; }
+        [Required(ErrorMessage = "Stock Name can't be blank")]
+        public string? StockName { get;set; }
+        public DateTime DateAndTimeOfOrder { get; set; }
+        [Range(1, 100000,ErrorMessage = "Can sell maximum of 100000 shares in single order, Minimum is 1.")]
+        public uint Quantity { get; set; } 
+        [Range(1, 10000,ErrorMessage = "The maximum selling price of stock is 10000, Minimum is 1.")]
+        public double Price { get; set; }
+    }
+}
