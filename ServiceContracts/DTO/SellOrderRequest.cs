8b3cfc7cf2c6f60aae45b6c264f1d6673207264b
nbondili4 <nbondili4@US-C4B79S3>
2023-12-11 12:52:50 +0530

    Created SellOrderRequest.cs in ServiceContracts Project

---
+++ ServiceContracts/DTO/SellOrderRequest.cs
@@ -0,0 +1,41 @@
+﻿using Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceContracts.DTO
+{
+    public class SellOrderRequest:IValidatableObject
+    {
+        [Required(ErrorMessage = "Stock Symbol can't be blank")]
+        public string? StockSymbol { get; set; }
+        [Required(ErrorMessage = "Stock Name can't be blank")]
+        public string? StockName { get; set; }
+        public DateTime DateAndTimeOfOrder { get; set; }
+        [Range(1, 100000, ErrorMessage = "Can sell maximum of 100000 shares in single order, Minimum is 1.")]
+        public uint Quantity { get; set; }
+        [Range(1, 10000, ErrorMessage = "The maximum price of stock is 10000, Minimum is 1.")]
+        public double Price { get; set; }
+
+        public SellOrder ToSellOrder()
+        {
+            return new SellOrder()
+            {
+                StockSymbol = StockSymbol,
+                StockName = StockName,
+                DateAndTimeOfOrder = DateAndTimeOfOrder,
+                Quantity = Quantity,
+                Price = Price
+            };
+        }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            //Date of order should be less than Jan 01, 2000
+            if (DateAndTimeOfOrder < Convert.ToDateTime("2000-01-01"))
+            {
+                results.Add(new ValidationResult("Date of the order should not be older than Jan 01, 2000."));
+            }
+            return results;
+        }
+    }
+}
