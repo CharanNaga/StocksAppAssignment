468bf4d0a16a35813723df7515f9acf0d54f5580
nbondili4 <nbondili4@US-C4B79S3>
2023-12-13 14:54:36 +0530

    Created a new Model "Orders.cs"

---
+++ StocksAppAssignment/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using ServiceContracts.DTO;
+
+namespace StocksAppAssignment.Models
+{
+    /// <summary>
+    /// Represents model class to supply list of buy orders and sell orders to the Trade/Orders view
+    /// </summary>
+    public class Orders
+    {
+        public List<BuyOrderResponse> BuyOrders { get; set; } = new List<BuyOrderResponse>();
+
+        public List<SellOrderResponse> SellOrders { get; set; } = new List<SellOrderResponse>();
+    }
+}
