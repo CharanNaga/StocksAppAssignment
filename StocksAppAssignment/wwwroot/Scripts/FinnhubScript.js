239eefd59dadfa9ae976cb5e11d9dc55a91c0fc3
nbondili4 <nbondili4@US-C4B79S3>
2023-12-17 15:30:49 +0530

    Modified finnhub script in webroot folder

--- StocksAppAssignment/wwwroot/Scripts/FinnhubScript.js
+++ StocksAppAssignment/wwwroot/Scripts/FinnhubScript.js
@@ -2,6 +2,7 @@
 const token = document.querySelector("#FinnhubToken").value;
 const socket = new WebSocket(`wss://ws.finnhub.io?token=${token}`);
 var stockSymbol = document.getElementById("StockSymbol").value; //get symbol from input hidden
+var counter = 0;
 
 // Connection opened. Subscribe to a symbol
 socket.addEventListener('open', function (event) {
@@ -37,7 +38,20 @@ socket.addEventListener('message', function (event) {
             //get the updated price
             var updatedPrice = JSON.parse(event.data).data[0].p;
             var timeStamp = JSON.parse(event.data).data[0].t;
-            //console.log(updatedPrice);
+            //console.log(updatedPrice, timeStamp);
+            //console.log(new Date(timeStamp).toLocaleTimeString());
+
+            //update the chart for every 6 server events
+            if (counter == 0 || counter % 6 == 0) {
+                //add price to prices array to reflect the same in chart
+                prices.push(updatedPrice);
+
+                //add time to labels array to reflect the same in chart
+                var timeStampDate = new Date(timeStamp);
+                labels.push(timeStampDate.toLocaleTimeString());
+                chart.update();
+            }
+            counter++;
 
             //update the UI
             $(".price").text(updatedPrice.toFixed(2)); //price - big display
