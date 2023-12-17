24802cb0eac8ccc0c4d569a10d17934359c43896
nbondili4 <nbondili4@US-C4B79S3>
2023-12-17 15:21:11 +0530

    To the constructor of Dbcontext class, passed the Dbcontext options and to the parent class constructor

--- Entities/StockMarketDbContext.cs
+++ Entities/StockMarketDbContext.cs
@@ -9,6 +9,10 @@ namespace Entities
 {
     public class StockMarketDbContext:DbContext
     {
+        public StockMarketDbContext(DbContextOptions options):base(options)
+        {
+
+        }
         public DbSet<BuyOrder> BuyOrders { get; set; }
         public DbSet<SellOrder> SellOrders { get;set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
