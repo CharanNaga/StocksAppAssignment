3211a4a60e21c5e6a09c93a17c795f7947de5a9d
nbondili4 <nbondili4@US-C4B79S3>
2023-12-17 13:38:21 +0530

    Changed ServiceLifetimes of Services added & Added a service with DbContext in Program.cs

--- StocksAppAssignment/Program.cs
+++ StocksAppAssignment/Program.cs
@@ -1,12 +1,22 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
 using ServiceContracts;
 using Services;
 using StocksAppAssignment;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
-builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
-builder.Services.AddSingleton<IStocksService, StocksService>();
+
+builder.Services.AddScoped<IFinnhubService, FinnhubService>();
+builder.Services.AddScoped<IStocksService, StocksService>();
+
+builder.Services.AddDbContext<StockMarketDbContext>(options =>
+{
+    options.UseSqlServer();
+});
+
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
