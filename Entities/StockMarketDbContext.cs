737033699e44747dc8db0fb49242c1684ade62bb
nbondili4 <nbondili4@US-C4B79S3>
2023-12-19 21:41:10 +0530

    Modified DbContextClass to make Dbset's as virtual and nullable to override in further Mock's & to remove warnings

--- Entities/StockMarketDbContext.cs
+++ Entities/StockMarketDbContext.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Entities
 {
     public class StockMarketDbContext:DbContext
@@ -13,8 +7,8 @@ namespace Entities
         {
 
         }
-        public DbSet<BuyOrder> BuyOrders { get; set; }
-        public DbSet<SellOrder> SellOrders { get;set; }
+        public virtual DbSet<BuyOrder>? BuyOrders { get; set; }
+        public virtual DbSet<SellOrder>? SellOrders { get;set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
