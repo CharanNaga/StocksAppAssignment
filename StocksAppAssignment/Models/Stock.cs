b7c64f58c58a5a6369729c49ae9634f33752b2d7
nbondili4 <nbondili4@US-C4B79S3>
2023-12-20 01:46:08 +0530

    Overriden Equals() in Stock.cs

--- StocksAppAssignment/Models/Stock.cs
+++ StocksAppAssignment/Models/Stock.cs
@@ -5,5 +5,18 @@
         public string? StockSymbol { get; set; }
 
         public string? StockName { get; set; }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not Stock) return false;
+
+            Stock other = (Stock)obj;
+            return StockSymbol == other.StockSymbol && StockName == other.StockName;
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
