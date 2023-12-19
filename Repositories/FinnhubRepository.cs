15f8d7d96811ad2ac390c0f5c064a407a2e399d1
nbondili4 <nbondili4@US-C4B79S3>
2023-12-19 22:48:58 +0530

    Implemented GetStockPriceQuote() repository method in FinnhubRepository.cs

--- Repositories/FinnhubRepository.cs
+++ Repositories/FinnhubRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using RepositoryContracts;
+using System.Text.Json;
 
 namespace Repositories
 {
@@ -19,9 +20,30 @@ namespace Repositories
             throw new NotImplementedException();
         }
 
-        public Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
         {
-            throw new NotImplementedException();
+            using (HttpClient httpClient = _httpClientFactory.CreateClient()) //creates and returns object of HttpClient class and place it in using block such that it disposes object at the end.
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage() //represent actual http request.
+                {
+                    RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}"), //pasting the api key for stocks price-> quotes curl from finnhub.io
+                    Method = HttpMethod.Get
+                };
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage); //our server acts as a client to external source, so need to receive response. Sends request to finnhub.io and validates token provided. If valid, provides the response.
+
+                //read responsebody
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                //Converting Json into a dictionary object.
+                Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+                if (responseDictionary == null)
+                    throw new InvalidOperationException("No response from finnhub server");
+
+                if (responseDictionary.ContainsKey("error"))
+                    throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+                return responseDictionary;
+            }
         }
 
         public Task<List<Dictionary<string, string>>?> GetStocks()
