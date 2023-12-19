6c0e6075ac19d270505606ea8d19b14e5d3c53a4
nbondili4 <nbondili4@US-C4B79S3>
2023-12-19 22:23:33 +0530

    Created FinnhubRepository.cs in Repositories Project & provided dummy implementation

---
+++ Repositories/FinnhubRepository.cs
@@ -0,0 +1,27 @@
+﻿using RepositoryContracts;
+
+namespace Repositories
+{
+    public class FinnhubRepository : IFinnhubRepository
+    {
+        public Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Dictionary<string, string>>?> GetStocks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
