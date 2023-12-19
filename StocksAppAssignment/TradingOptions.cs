d56e5885eb202bd20c8325c458e672e3ba9bdcdb
nbondili4 <nbondili4@US-C4B79S3>
2023-12-19 23:54:54 +0530

    Removed DefaultStockSymbol property & Added Top25PopularStocks property in TradingOptions.cs

--- StocksAppAssignment/TradingOptions.cs
+++ StocksAppAssignment/TradingOptions.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public class TradingOptions
     {
-        public string? DefaultStockSymbol { get; set; }
         public uint? DefaultOrderQuantity { get; set; }
+        public string? Top25PopularStocks {get;set;}
     }
 }
