4de90beec2a1423ca1f1040ff2cdb95e50135600
nbondili4 <nbondili4@US-C4B79S3>
2023-12-19 22:52:25 +0530

    Implemented GetCompanyProfile() method in FinnhubRepository.cs

--- Repositories/FinnhubRepository.cs
+++ Repositories/FinnhubRepository.cs
@@ -15,9 +15,37 @@ namespace Repositories
             _configuration = configuration;
         }
 
-        public Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
+        public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
         {
-            throw new NotImplementedException();
+            //create http client
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+
+                //create http request
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token={_configuration["FinnhubToken"]}") //URI includes the secret token
+                };
+
+                //send request
+                HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                //read response body
+                string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+                //convert response body (from JSON into Dictionary)
+                Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+                if (responseDictionary == null)
+                    throw new InvalidOperationException("No response from server");
+
+                if (responseDictionary.ContainsKey("error"))
+                    throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+                //return response dictionary back to the caller
+                return responseDictionary;
+            }
         }
 
         public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
