6a5d0ebec630e16cd8431b33d76b21ddc804649d
nbondili4 <nbondili4@US-C4B79S3>
2023-12-20 00:28:59 +0530

    Created StocksController & initialized private fields in Constructor

---
+++ StocksAppAssignment/Controllers/StocksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiceContracts;
+
+namespace StocksAppAssignment.Controllers
+{
+    [Route("[controller]")]
+    public class StocksController : Controller
+    {
+        private readonly TradingOptions _tradingOptions;
+        private readonly IStocksService _stocksService;
+
+        public StocksController(IOptions<TradingOptions> options, IStocksService stocksService)
+        {
+            _tradingOptions = options.Value;
+            _stocksService = stocksService;
+        }
+
+
+        public IActionResult Explore()
+        {
+            return View();
+        }
+    }
+}
