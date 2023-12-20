72e1c72910e83bd552a832c29721ffc6ba766b0f
nbondili4 <nbondili4@US-C4B79S3>
2023-12-20 06:15:33 +0530

    Created CustomWebApplicationFactory.cs in Tests

---
+++ Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.UseEnvironment("Test");
+
+            builder.ConfigureServices(services =>
+            { 
+                var descriptor = services.SingleOrDefault(
+                    temp=>temp.ServiceType == typeof(ApplicationDbContext)
+                    );
+                if(descriptor != null )
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("DbForIntegrationTesting");
+                });
+            });
+        }
+    }
+}
