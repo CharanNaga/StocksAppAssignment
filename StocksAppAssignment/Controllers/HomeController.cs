ebc39a5c60200320bb1dfe52203e214132a3153a
nbondili4 <nbondili4@US-C4B79S3>
2023-12-29 17:11:27 +0530

    Created HomeController with Route Specified in built-in exception handler

---
+++ StocksAppAssignment/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StocksAppAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>(); 
+            if(exceptionHandlerPathFeature != null && exceptionHandlerPathFeature.Error != null)
+            {
+                ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Error Occured";
+            }
+            return View();
+        }
+    }
+}
