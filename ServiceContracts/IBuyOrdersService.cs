1eeba0ee10fa8fab881a45bf321c2506a0e13a37
nbondili4 <nbondili4@US-C4B79S3>
2023-12-30 01:49:36 +0530

    Splitted IStocksService into 2 seperate interfaces for satisfying ISP

---
+++ ServiceContracts/IBuyOrdersService.cs
@@ -0,0 +1,22 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents Stocks service that includes operations like buy order and get buy orders
+    /// </summary>
+    public interface IBuyOrdersService
+    {
+        /// <summary>
+        /// Creates a buy order
+        /// </summary>
+        /// <param name="buyOrderRequest">Buy order object</param>
+        Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest);
+
+        /// <summary>
+        /// Returns all existing buy orders
+        /// </summary>
+        /// <returns>Returns a list of objects of BuyOrder type</returns>
+        Task<List<BuyOrderResponse>> GetBuyOrders();
+    }
+}
