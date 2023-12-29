38e6a7c89a8382aa877d981c21650d3f06afa55d
nbondili4 <nbondili4@US-C4B79S3>
2023-12-29 17:06:57 +0530

    Modified FinnhubService class in which all service methods are made use of custom exception class if they are unable to connect to finnhub api

--- Services/FinnhubService.cs
+++ Services/FinnhubService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Exceptions;
+using Microsoft.Extensions.Logging;
 using RepositoryContracts;
 using ServiceContracts;
 
@@ -16,30 +17,58 @@ namespace Services
 
         public async Task<Dictionary<string, object>?> GetCompanyProfile(string stockSymbol)
         {
-            //writing log message
-            _logger.LogInformation($"In {nameof(FinnhubService)}.{nameof(GetCompanyProfile)} Service method");
-            Dictionary<string,object>? responseDictionary = await _finnhubRepository.GetCompanyProfile(stockSymbol);
-            return responseDictionary;
+            try
+            {
+                //writing log message
+                _logger.LogInformation($"In {nameof(FinnhubService)}.{nameof(GetCompanyProfile)} Service method");
+                Dictionary<string, object>? responseDictionary = await _finnhubRepository.GetCompanyProfile(stockSymbol);
+                return responseDictionary;
+            }
+            catch (Exception ex)
+            {
+                throw new FinnhubException("Unable to connect to finnhub ",ex);
+            }
         }
 
         public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
         {
-            //writing log message
-            _logger.LogInformation($"In {nameof(FinnhubService)}.{nameof(GetStockPriceQuote)} Service method");
-            Dictionary<string,object>? responseDictionary = await _finnhubRepository.GetStockPriceQuote(stockSymbol);
-            return responseDictionary;
+            try
+            {
+                //writing log message
+                _logger.LogInformation($"In {nameof(FinnhubService)}.{nameof(GetStockPriceQuote)} Service method");
+                Dictionary<string, object>? responseDictionary = await _finnhubRepository.GetStockPriceQuote(stockSymbol);
+                return responseDictionary;
+            }
+            catch(Exception ex)
+            {
+                throw new FinnhubException("Unable to connect to finnhub ", ex);
+            }
         }
 
         public async Task<List<Dictionary<string, string>>?> GetStocks()
         {
-            List<Dictionary<string, string>>? responseDictionaryList  = await _finnhubRepository.GetStocks();
-            return responseDictionaryList;
+            try
+            {
+                List<Dictionary<string, string>>? responseDictionaryList = await _finnhubRepository.GetStocks();
+                return responseDictionaryList;
+            }
+            catch(Exception ex)
+            {
+                throw new FinnhubException("Unable to connect to finnhub", ex);
+            }
         }
 
         public async Task<Dictionary<string, object>?> SearchStocks(string stockSymbolToSearch)
         {
-            Dictionary<string, object>? responseDictionary = await _finnhubRepository.SearchStocks(stockSymbolToSearch);
-            return responseDictionary;
+            try
+            {
+                Dictionary<string, object>? responseDictionary = await _finnhubRepository.SearchStocks(stockSymbolToSearch);
+                return responseDictionary;
+            }
+            catch(Exception ex)
+            {
+                throw new FinnhubException("Unable to connect to finnhub ", ex);
+            }
         }
     }
 }
